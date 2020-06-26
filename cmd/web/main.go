62dc7826f8bdfc4e2b7e53649de2d5fa3c21ebeb
Vladimir Stepanov <vovakirishi@gmail.com>
2020-06-26 14:20:15 +0300

    Print error from Start

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,18 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	addr := flag.String("addr", ":8080", "Listen addr")
 
 	flag.Parse()
 	serv := New(*addr)
-	serv.Start()
+
+	if err := serv.Start(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
 
 }

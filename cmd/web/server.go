8411d6d57f64909c01efe5a8bdef265427b94977
Vladimir Stepanov <vovakirishi@gmail.com>
2020-06-26 13:49:46 +0300

    Add static routes

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -14,15 +15,24 @@ type Server struct {
 func (s *Server) routes() *mux.Router {
 	r := mux.NewRouter()
 
+	strPref := http.StripPrefix("/static/", http.FileServer(http.Dir("./ui/static/")))
+	r.PathPrefix("/static/").Handler(strPref)
 	r.HandleFunc("/", s.home).Methods("GET")
-
 	return r
 }
 
 //Start listen and serve
 func (s *Server) Start() error {
 
-	return http.ListenAndServe(":8080", s.routes())
+	srv := &http.Server{
+		Handler: s.routes(),
+		Addr:    ":8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	return srv.ListenAndServe()
 }
 
 //New return new Server instance

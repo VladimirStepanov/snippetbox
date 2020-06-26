506f4eb6815335e22931bfe319fb67f9c393cf97
Vladimir Stepanov <vovakirishi@gmail.com>
2020-06-26 13:09:25 +0300

    Start func

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"net/http"
-)
-
 func main() {
 
 	serv := New()
 
-	http.ListenAndServe(":8080", serv.Routes())
+	serv.Start()
 
 }
--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 //Server apllication struct
 type Server struct {
+}
+
+//Routes return mux.Router with filled routes
+func (s *Server) routes() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", s.home).Methods("GET")
+
+	return r
+}
+
+//Start listen and serve
+func (s *Server) Start() error {
+
+	return http.ListenAndServe(":8080", s.routes())
 }
 
 //New return new Server instance

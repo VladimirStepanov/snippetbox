aa39a82cb1921581f832465fd02d68a5f6c6fc4c
Vladimir Stepanov <vovakirishi@gmail.com>
2020-06-28 00:53:56 +0300

    TestAddDefaultTemplateData for test default args

--- cmd/web/template_test.go
+++ cmd/web/template_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestRenderNotFound(t *testing.T) {
@@ -13,7 +16,7 @@ func TestRenderNotFound(t *testing.T) {
 
 	w := httptest.NewRecorder()
 
-	s.render(w, "broken", nil)
+	s.render(w, "broken", &templateData{Title: "TestPage"})
 
 	if w.Code != http.StatusInternalServerError {
 		t.Fatalf("Error! %d != %d", w.Code, http.StatusInternalServerError)
@@ -29,9 +32,30 @@ func TestRenderSuccess(t *testing.T) {
 
 	w := httptest.NewRecorder()
 
-	s.render(w, "snippets", nil)
+	s.render(w, "snippets", &templateData{Title: "TestPage"})
 
 	if w.Code != http.StatusOK {
 		t.Fatalf("Error! %d != %d", w.Code, http.StatusOK)
+	}
+}
+
+func TestAddDefaultTemplateData(t *testing.T) {
+	title := "FFFFFPAGE"
+	s, err := NewTestServerWithUI("../../ui/html")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	s.render(w, "snippets", &templateData{Title: title})
+
+	if !strings.Contains(w.Body.String(), title) {
+		t.Fatalf("Body doesn't contain %s", title)
+	}
+
+	if !strings.Contains(w.Body.String(), fmt.Sprintf("%d", time.Now().Year())) {
+		t.Fatalf("Body doesn't contain current year %d", time.Now().Year())
 	}
 }

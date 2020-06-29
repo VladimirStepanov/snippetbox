2287020a48799a59dff552bfbfe17d618293abd9
Vladimir Stepanov <vovakirishi@gmail.com>
2020-06-30 00:01:38 +0300

    Add openDB func

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"runtime/debug"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
 func (s *Server) serverError(w http.ResponseWriter, err error) {
 	s.log.Errorf("Internal error: %v %s", err, string(debug.Stack()))
 	http.Error(w, "Internal error", http.StatusInternalServerError)

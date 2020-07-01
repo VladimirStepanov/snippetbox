f19329c4a6bff8b1f2b1a87621833329d7958abf
Vladimir Stepanov <vovakirishi@gmail.com>
2020-07-01 15:22:15 +0300

    Test helper for mysql package

---
+++ pkg/models/mysql/test_helper.go
@@ -0,0 +1,25 @@
+package mysql
+
+import (
+	"database/sql"
+	"testing"
+)
+
+// GetDB ...
+func GetDB(t *testing.T, dsn string) (*sql.DB, func(tables ...string)) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return db, func(tables ...string) {
+		for _, name := range tables {
+			_, err := db.Exec("DELETE FROM  " + name)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		defer db.Close()
+	}
+
+}

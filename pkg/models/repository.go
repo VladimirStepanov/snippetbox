b2eaf04615f0ab0d7fcad894075ff315045d99bc
Vladimir Stepanov <vovakirishi@gmail.com>
2020-07-03 17:03:28 +0300

    Add User and Snippet repository

---
+++ pkg/models/repository.go
@@ -0,0 +1,17 @@
+package models
+
+//UserRepository interface for working with DB
+type UserRepository interface {
+	Insert(firstname, lastname, mail, password string) (int64, error)
+	Get(id int64) (*User, error)
+	Authenticate(email, password string) (int64, error)
+}
+
+//SnippetRepository interface for working with DB
+type SnippetRepository interface {
+	Insert(title, content string, expire int, isPublic bool, ownerID int64) (int64, error)
+	Delete(snippetID int64) error
+	Get(snippetID, userID int64) (*Snippet, error)
+	Update(snippet *Snippet, ownerID int64) error
+	LatestAll(ownerID int64, count, page int) ([]*Snippet, error)
+}

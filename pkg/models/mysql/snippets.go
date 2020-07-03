e5999ffe2d103a1916afb9d5e374e751ddf6f58e
Vladimir Stepanov <vovakirishi@gmail.com>
2020-07-03 14:12:33 +0300

    Add SnippetStore and tests for this

---
+++ pkg/models/mysql/snippets.go
@@ -0,0 +1,167 @@
+package mysql
+
+import (
+	"database/sql"
+	"fmt"
+
+	"githib.com/VladimirStepanov/snippetbox/pkg/models"
+	"github.com/go-sql-driver/mysql"
+)
+
+//SnippetStore struct for working with snippets table
+type SnippetStore struct {
+	DB *sql.DB
+}
+
+// Insert snippet into database
+func (s *SnippetStore) Insert(title, content string, expire int, isPublic bool, ownerID int64) (int64, error) {
+	res, err := s.DB.Exec(
+		`INSERT into snippets (title, content, create_date, expiration_date, is_public, owner_id) 
+		VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY), ?, ?)`,
+		title,
+		content,
+		expire,
+		isPublic,
+		ownerID,
+	)
+
+	if err != nil {
+		if me, ok := err.(*mysql.MySQLError); ok {
+			if me.Number == 1452 {
+				return 0, models.ErrUnknownOwnerID
+			}
+		}
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+//Delete from snippets
+func (s *SnippetStore) Delete(snippetID int64) error {
+	res, err := s.DB.Exec("DELETE from snippets WHERE id=?", snippetID)
+
+	if err != nil {
+		return err
+	}
+
+	count, err := res.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if count == 0 {
+		return models.ErrNoRecord
+	}
+
+	return nil
+
+}
+
+//Get specific snippet
+func (s *SnippetStore) Get(snippetID, userID int64) (*models.Snippet, error) {
+	res := &models.Snippet{}
+	row := s.DB.QueryRow(
+		`SELECT id, title, content, create_date, expiration_date, is_public, owner_id from snippets 
+		WHERE id=? AND owner_id = ? AND expiration_date > CURDATE()`,
+		snippetID,
+		userID,
+	)
+
+	err := row.Scan(&res.ID, &res.Title, &res.Content, &res.Created, &res.Expires, &res.IsPublic, &res.OwnerID)
+
+	if err == sql.ErrNoRows {
+		return nil, models.ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+//Update snippet
+func (s *SnippetStore) Update(snippet *models.Snippet, ownerID int64) error {
+	res, err := s.DB.Exec(
+		"update snippets set title = ?, content = ?, is_public = ? where id = ? and owner_id = ?",
+		snippet.Title,
+		snippet.Content,
+		snippet.IsPublic,
+		snippet.ID,
+		ownerID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	ra, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if ra == 0 {
+		return models.ErrNoRecord
+	}
+
+	return nil
+}
+
+func (s *SnippetStore) getSnippets(rows *sql.Rows) ([]*models.Snippet, error) {
+
+	snippets := []*models.Snippet{}
+
+	for rows.Next() {
+		res := &models.Snippet{}
+
+		err := rows.Scan(&res.ID, &res.Title, &res.Content, &res.Created, &res.Expires, &res.IsPublic, &res.OwnerID)
+
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, res)
+	}
+
+	return snippets, nil
+}
+
+//LatestAll return latest snippets sorted by create_date
+func (s *SnippetStore) LatestAll(ownerID int64, count, page int) ([]*models.Snippet, error) {
+	var rows *sql.Rows
+	var err error
+	var limit string
+
+	if page == 1 {
+		limit = fmt.Sprintf("LIMIT %d", count)
+	} else {
+		limit = fmt.Sprintf("LIMIT %d, %d", count*page-count, count)
+	}
+	if ownerID == -1 {
+		rows, err = s.DB.Query(
+			`SELECT id, title, content, create_date, expiration_date, is_public, owner_id from snippets 
+			WHERE expiration_date > CURDATE() AND is_public = 1 ORDER BY create_date DESC ` + limit,
+		)
+	} else {
+		rows, err = s.DB.Query(
+			`SELECT id, title, content, create_date, expiration_date, is_public, owner_id from snippets
+			WHERE expiration_date > CURDATE() AND owner_id = ? ORDER BY create_date DESC `+limit, ownerID,
+		)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	res, err := s.getSnippets(rows)
+
+	return res, err
+}
---
+++ pkg/models/mysql/snippets_test.go
@@ -0,0 +1,386 @@
+package mysql
+
+import (
+	"database/sql"
+	"testing"
+
+	"githib.com/VladimirStepanov/snippetbox/pkg/models"
+)
+
+type SnippetData struct {
+	Title    string
+	Content  string
+	Expire   int
+	IsPublic bool
+}
+
+func getPreparedSnippetStore(t *testing.T, db *sql.DB) (*SnippetStore, int64) {
+	us := &UsersStore{DB: db}
+
+	userID, err := us.Insert("test", "test", "test", "test")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return &SnippetStore{DB: db}, userID
+}
+
+func TestDeleteSnippet(t *testing.T) {
+	tests := map[string]struct {
+		WantError error
+		Data      *SnippetData
+		GetID     func(id int64) int64
+	}{
+		"Delete row not found": {
+			WantError: models.ErrNoRecord,
+			Data:      nil,
+			GetID: func(id int64) int64 {
+				return id + 10
+			},
+		},
+		"Success delete": {
+			WantError: nil,
+			Data: &SnippetData{
+				Title:   "Title",
+				Content: "Content",
+				Expire:  1,
+			},
+			GetID: func(id int64) int64 {
+				return id
+			},
+		},
+	}
+
+	for name, value := range tests {
+		t.Run(name, func(t *testing.T) {
+			db, truncate := GetDB(t, "root:123@/snippetbox_test")
+			ss, ownerID := getPreparedSnippetStore(t, db)
+			defer truncate("snippets", "users")
+
+			var snippetID int64
+			var err error
+
+			if value.Data != nil {
+				snippetID, err = ss.Insert(value.Data.Title, value.Data.Content, value.Data.Expire, value.Data.IsPublic, ownerID)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			err = ss.Delete(value.GetID(snippetID))
+
+			if value.WantError != nil && value.WantError != err {
+				t.Fatalf("Want: %v, Get: %v\n", value.WantError, err)
+			}
+
+			if value.WantError == nil && err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+
+}
+
+func TestInsertSnippet(t *testing.T) {
+	tests := map[string]struct {
+		WantError  error
+		Data       *SnippetData
+		GetOwnerID func(id int64) int64
+	}{
+		"Success insert": {
+			WantError: nil,
+			Data: &SnippetData{
+				Title:   "Title",
+				Content: "Content",
+				Expire:  1,
+			},
+			GetOwnerID: func(id int64) int64 {
+				return id
+			},
+		},
+		"Unknown owner ID": {
+			WantError: models.ErrUnknownOwnerID,
+			Data: &SnippetData{
+				Title:   "Title",
+				Content: "Content",
+				Expire:  1,
+			},
+			GetOwnerID: func(id int64) int64 {
+				return id + 10
+			},
+		},
+	}
+	for name, value := range tests {
+		t.Run(name, func(t *testing.T) {
+			db, truncate := GetDB(t, "root:123@/snippetbox_test")
+			ss, userID := getPreparedSnippetStore(t, db)
+			defer truncate("snippets", "users")
+
+			_, err := ss.Insert(value.Data.Title, value.Data.Content, value.Data.Expire, value.Data.IsPublic, value.GetOwnerID(userID))
+
+			if value.WantError != nil && value.WantError != err {
+				t.Fatalf("Want: %v, Get: %v\n", value.WantError, err)
+			}
+
+			if value.WantError == nil && err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+
+}
+
+func TestUpdate(t *testing.T) {
+	tests := map[string]struct {
+		WantError             error
+		Data                  *SnippetData
+		GetSnippetAfterUpdate func(s *SnippetData) *SnippetData
+	}{
+		"Update ErrNoRecord": {
+			WantError: models.ErrNoRecord,
+			Data:      nil,
+			GetSnippetAfterUpdate: func(s *SnippetData) *SnippetData {
+				return &SnippetData{}
+			},
+		},
+		"Update success": {
+			WantError: nil,
+			Data: &SnippetData{
+				Title:   "Title",
+				Content: "Content",
+				Expire:  1,
+			},
+			GetSnippetAfterUpdate: func(s *SnippetData) *SnippetData {
+				s.Title += "Hello world"
+				return s
+			},
+		},
+	}
+
+	for name, value := range tests {
+		t.Run(name, func(t *testing.T) {
+			db, truncate := GetDB(t, "root:123@/snippetbox_test?parseTime=true")
+			ss, ownerID := getPreparedSnippetStore(t, db)
+			defer truncate("snippets", "users")
+
+			var snippetID int64
+			var err error
+
+			if value.Data != nil {
+				snippetID, err = ss.Insert(
+					value.Data.Title, value.Data.Content, value.Data.Expire, value.Data.IsPublic, ownerID,
+				)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			updatedSnippet := value.GetSnippetAfterUpdate(value.Data)
+
+			err = ss.Update(
+				&models.Snippet{ID: snippetID,
+					Title:    updatedSnippet.Title,
+					Content:  updatedSnippet.Content,
+					IsPublic: updatedSnippet.IsPublic,
+				},
+				ownerID,
+			)
+
+			if value.WantError != nil && value.WantError != err {
+				t.Fatalf("Want: %v, Get: %v\n", value.WantError, err)
+			}
+
+			if value.WantError == nil && err != nil {
+				t.Fatal(err)
+			}
+
+			if value.Data != nil && snippetID != 0 {
+				snippet, err := ss.Get(snippetID, ownerID)
+
+				if err != nil {
+					t.Fatalf("Error while get: %v %d %d", err, snippetID, ownerID)
+				}
+
+				if snippet.Content != updatedSnippet.Content || snippet.IsPublic != updatedSnippet.IsPublic || snippet.Title != updatedSnippet.Title {
+					t.Fatalf("Want: %v, Get: %v", snippet, updatedSnippet)
+				}
+			}
+
+		})
+	}
+}
+
+func TestGetSnippet(t *testing.T) {
+	tests := map[string]struct {
+		WantError error
+		Data      *SnippetData
+		GetID     func(id int64) int64
+	}{
+		"Get ErrNoRecord": {
+			WantError: models.ErrNoRecord,
+			Data:      nil,
+		},
+		"Success get": {
+			WantError: nil,
+			Data: &SnippetData{
+				Title:   "Title",
+				Content: "Content",
+				Expire:  1,
+			},
+		},
+		"Snippet expire": {
+			WantError: models.ErrNoRecord,
+			Data: &SnippetData{
+				Title:   "Title",
+				Content: "Content",
+				Expire:  -1,
+			},
+		},
+	}
+
+	for name, value := range tests {
+		t.Run(name, func(t *testing.T) {
+			db, truncate := GetDB(t, "root:123@/snippetbox_test?parseTime=true")
+			ss, ownerID := getPreparedSnippetStore(t, db)
+			defer truncate("snippets", "users")
+
+			var snippetID int64
+			var err error
+
+			if value.Data != nil {
+				snippetID, err = ss.Insert(value.Data.Title, value.Data.Content, value.Data.Expire, value.Data.IsPublic, ownerID)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			snippet, err := ss.Get(snippetID, ownerID)
+
+			if value.WantError != nil && value.WantError != err {
+				t.Fatalf("Want: %v, Get: %v\n", value.WantError, err)
+			}
+
+			if value.WantError == nil && err != nil {
+				t.Fatal(err)
+			}
+
+			if snippet != nil && value.Data != nil && value.WantError == nil {
+				if snippet.Content != value.Data.Content || snippet.IsPublic != value.Data.IsPublic || snippet.Title != value.Data.Title || snippet.OwnerID != ownerID {
+					t.Fatalf("Want: %v, Get: %v", snippet, value.Data)
+				}
+			}
+		})
+	}
+}
+
+func TestLatestAll(t *testing.T) {
+	snippets := []*SnippetData{
+		{"1", "2", 1, true},
+		{"11", "22", 1, false},
+		{"111", "222", 1, true},
+		{"exp11", "exp22", -1, false},
+	}
+
+	tests := map[string]struct {
+		WantError  error
+		WantResult []*SnippetData
+		WantLatest int
+		WantPage   int
+		GetOwnerID func(int64) int64
+	}{
+		"All public": {
+			WantError:  nil,
+			WantResult: []*SnippetData{snippets[0], snippets[2]},
+			WantLatest: 5,
+			WantPage:   1,
+			GetOwnerID: func(id int64) int64 {
+				return -1
+			},
+		},
+		"All for owner ID": {
+			WantError:  nil,
+			WantResult: snippets[:3],
+			WantLatest: 5,
+			WantPage:   1,
+			GetOwnerID: func(id int64) int64 {
+				return id
+			},
+		},
+		"Empty latest": {
+			WantError:  nil,
+			WantResult: []*SnippetData{},
+			WantLatest: 5,
+			WantPage:   1,
+			GetOwnerID: func(id int64) int64 {
+				return id + 3
+			},
+		},
+		"Test first page": {
+			WantError:  nil,
+			WantResult: snippets[:2],
+			WantLatest: 2,
+			WantPage:   1,
+			GetOwnerID: func(id int64) int64 {
+				return id
+			},
+		},
+		"Test last page": {
+			WantError:  nil,
+			WantResult: []*SnippetData{snippets[2]},
+			WantLatest: 2,
+			WantPage:   2,
+			GetOwnerID: func(id int64) int64 {
+				return id
+			},
+		},
+		"Test third page": {
+			WantError:  nil,
+			WantResult: []*SnippetData{snippets[2]},
+			WantLatest: 1,
+			WantPage:   3,
+			GetOwnerID: func(id int64) int64 {
+				return id
+			},
+		},
+	}
+
+	for name, value := range tests {
+		t.Run(name, func(t *testing.T) {
+			db, truncate := GetDB(t, "root:123@/snippetbox_test?parseTime=true")
+			ss, ownerID := getPreparedSnippetStore(t, db)
+			defer truncate("snippets", "users")
+
+			for _, snippet := range snippets {
+				_, err := ss.Insert(snippet.Title, snippet.Content, snippet.Expire, snippet.IsPublic, ownerID)
+				if err != nil {
+					t.Fatal(err)
+				}
+			}
+
+			latestSnippets, err := ss.LatestAll(value.GetOwnerID(ownerID), value.WantLatest, value.WantPage)
+			if value.WantError != nil && value.WantError != err {
+				t.Fatalf("Want: %v, Get: %v\n", value.WantError, err)
+			}
+
+			if value.WantError == nil && err != nil {
+				t.Fatal(err)
+			}
+
+			if len(value.WantResult) > 0 {
+				if len(value.WantResult) != len(latestSnippets) {
+					t.Fatalf("Want len %d, Got len: %d", len(value.WantResult), len(latestSnippets))
+				}
+
+				for i := range latestSnippets {
+					if latestSnippets[i].Content != value.WantResult[i].Content || latestSnippets[i].IsPublic != value.WantResult[i].IsPublic || latestSnippets[i].Title != value.WantResult[i].Title {
+						t.Fatalf("Want: %v, Get: %v", latestSnippets[i], value.WantResult[i])
+					}
+				}
+			} else if len(value.WantResult) == 0 && len(latestSnippets) != 0 {
+				t.Fatalf("Want len: 0, got len: %d", len(latestSnippets))
+			}
+
+		})
+	}
+}

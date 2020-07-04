dd5cda25eba83f64831dcc5978eb6baa2a30f0b9
Vladimir Stepanov <vovakirishi@gmail.com>
2020-07-04 18:34:13 +0300

    New signature of Delete function

--- pkg/models/repository.go
+++ pkg/models/repository.go
@@ -10,7 +10,7 @@ type UserRepository interface {
 //SnippetRepository interface for working with DB
 type SnippetRepository interface {
 	Insert(title, content string, expire int, isPublic bool, ownerID int64) (int64, error)
-	Delete(snippetID int64) error
+	Delete(snippetID, userID int64) error
 	Get(snippetID, userID int64) (*Snippet, error)
 	Update(snippet *Snippet, ownerID int64) error
 	LatestAll(ownerID int64, count, page int) ([]*Snippet, error)

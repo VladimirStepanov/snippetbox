db2170bb73bcccebf441806abbd7276489b82663
Vladimir Stepanov <vovakirishi@gmail.com>
2020-07-05 14:39:26 +0300

    Mock Users and Snippets for testing

---
+++ pkg/models/mock/snippets.go
@@ -0,0 +1,126 @@
+package mock
+
+import (
+	"math/rand"
+	"sort"
+	"time"
+
+	"githib.com/VladimirStepanov/snippetbox/pkg/models"
+)
+
+func remove(slice []*models.Snippet, s int) []*models.Snippet {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func getRandSnippetID(slice []*models.Snippet) int64 {
+	var found bool
+	for {
+		id := rand.Int63()
+		for _, v := range slice {
+			if v.ID == id {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return id
+		}
+	}
+}
+
+//SnippetStore mock for snippets
+type SnippetStore struct {
+	DB       []*models.Snippet
+	UsersMap map[int64]*models.User
+}
+
+//Insert snippet to map
+func (s *SnippetStore) Insert(title, content string, expire int, isPublic bool, ownerID int64) (int64, error) {
+	if _, ok := s.UsersMap[ownerID]; !ok {
+		return 0, models.ErrUnknownOwnerID
+	}
+
+	id := getRandSnippetID(s.DB)
+
+	s.DB = append(s.DB, &models.Snippet{
+		ID:       id,
+		Title:    title,
+		Content:  content,
+		Created:  time.Now(),
+		Expires:  time.Now().AddDate(0, 0, expire),
+		OwnerID:  ownerID,
+		IsPublic: isPublic,
+	})
+
+	return id, nil
+}
+
+//Get specific snippet
+func (s *SnippetStore) Get(snippetID, userID int64) (*models.Snippet, error) {
+	for _, value := range s.DB {
+		if value.ID == snippetID && value.OwnerID == userID && value.Expires.After(time.Now()) {
+			return value, nil
+		}
+	}
+
+	return nil, models.ErrNoRecord
+}
+
+//Delete from snippets
+func (s *SnippetStore) Delete(snippetID, userID int64) error {
+	for i, value := range s.DB {
+		if value.ID == snippetID && value.OwnerID == userID && value.Expires.After(time.Now()) {
+			remove(s.DB, i)
+			return nil
+		}
+	}
+
+	return models.ErrNoRecord
+}
+
+//Update from snippets
+func (s *SnippetStore) Update(snippet *models.Snippet, ownerID int64) error {
+	for _, value := range s.DB {
+		if value.ID == snippet.ID && value.OwnerID == ownerID && value.Expires.After(time.Now()) {
+			value.Title = snippet.Title
+			value.Content = snippet.Content
+			value.IsPublic = snippet.IsPublic
+			return nil
+		}
+	}
+
+	return models.ErrNoRecord
+}
+
+//LatestAll return latest snippets sorted by create_date
+func (s *SnippetStore) LatestAll(ownerID int64, count, page int) ([]*models.Snippet, error) {
+	var start int
+
+	start = page
+	if start == 1 {
+		start = 0
+	}
+
+	sort.SliceStable(s.DB, func(i, j int) bool {
+		return s.DB[i].Expires.Before(s.DB[j].Expires)
+	})
+	res := []*models.Snippet{}
+
+	for _, val := range s.DB[start:] {
+		if val.Expires.After(time.Now()) {
+			if ownerID == -1 && val.IsPublic {
+				res = append(res, val)
+			} else if ownerID != -1 && ownerID == val.OwnerID {
+				res = append(res, val)
+			}
+
+			if len(res) == count {
+				break
+			}
+		}
+	}
+
+	return res, nil
+
+}
---
+++ pkg/models/mock/users.go
@@ -0,0 +1,74 @@
+package mock
+
+import (
+	"math/rand"
+
+	"githib.com/VladimirStepanov/snippetbox/pkg/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//UsersStore mock for test endpoint
+type UsersStore struct {
+	DB map[int64]*models.User
+}
+
+func getRandUserID(m map[int64]*models.User) int64 {
+
+	for {
+		id := rand.Int63()
+		if _, ok := m[id]; !ok {
+			return id
+		}
+	}
+}
+
+//Insert user in map
+func (us *UsersStore) Insert(firstname, lastname, mail, password string) (int64, error) {
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+
+	if err != nil {
+		return 0, err
+	}
+
+	for _, value := range us.DB {
+		if value.Email == mail {
+			return 0, models.ErrDuplicateEmail
+		}
+	}
+
+	id := getRandUserID(us.DB)
+
+	us.DB[id] = &models.User{ID: id, Firstname: firstname, Lastname: lastname, Email: mail, HashedPassword: hashedPassword}
+
+	return id, nil
+}
+
+//Get User from map
+func (us *UsersStore) Get(id int64) (*models.User, error) {
+	if val, ok := us.DB[id]; ok {
+		val.HashedPassword = nil
+		return val, nil
+	}
+
+	return nil, models.ErrNoRecord
+}
+
+//Authenticate ...
+func (us *UsersStore) Authenticate(email, password string) (int64, error) {
+	for id, value := range us.DB {
+		if value.Email == email {
+			err := bcrypt.CompareHashAndPassword([]byte(value.HashedPassword), []byte(password))
+			if err == bcrypt.ErrMismatchedHashAndPassword {
+				return 0, models.ErrAuth
+			} else if err != nil {
+				return 0, err
+			}
+
+			return id, nil
+		}
+	}
+
+	return 0, models.ErrAuth
+
+}

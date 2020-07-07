c81769228fbe7a29a6c5098852c6e9bb14bbb8c3
Vladimir Stepanov <vovakirishi@gmail.com>
2020-07-07 19:13:36 +0300

    Private snippet code from 500 to 404

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"githib.com/VladimirStepanov/snippetbox/pkg/models"
+	"github.com/gorilla/mux"
 )
 
 func (s *Server) home(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +36,28 @@ func (s *Server) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	s.render(w, "snippets", &templateData{Title: "Home", Snippets: snippets})
+}
+
+func (s *Server) showSnippet(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	snippet, err := s.snippetStore.Get(int64(id))
+
+	if err != nil {
+		if err == models.ErrNoRecord {
+			http.NotFound(w, r)
+		} else {
+			s.serverError(w, err)
+		}
+		return
+	}
+
+	if !snippet.IsPublic {
+		// check for user session
+		http.NotFound(w, r)
+		return
+	}
+
+	s.render(w, "snippet", &templateData{Snippet: snippet})
 }
--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -99,3 +99,55 @@ func TestHomeHandlerWithData(t *testing.T) {
 		})
 	}
 }
+
+func TestShowSnippetForNotAuthUser(t *testing.T) {
+	um := getTestUserData()
+	// ss := getTestSnippetData(1, 15, true, 1)
+	ss := append(getTestSnippetData(1, 1, true, 1), getTestSnippetData(5, 3, false, 1)...)
+
+	s, err := NewTestServerWithUI("../../ui/html", &mock.SnippetStore{DB: ss, UsersMap: um}, &mock.UsersStore{DB: um})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	srv := httptest.NewServer(s.routes())
+
+	defer srv.Close()
+
+	tests := map[string]struct {
+		WantCode    int
+		WantID      int64
+		WantSnippet *models.Snippet
+	}{
+		"ShowSnippet": {
+			WantCode:    http.StatusOK,
+			WantID:      ss[0].ID,
+			WantSnippet: ss[0],
+		},
+		"Snippet not found": {
+			WantCode: http.StatusNotFound,
+			WantID:   100500,
+		},
+		"Get private snippet": {
+			WantCode: http.StatusNotFound,
+			WantID:   ss[3].ID,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			code, _, data := get(fmt.Sprintf("%s/snippet/%d", srv.URL, test.WantID), t, srv)
+
+			if test.WantCode != code {
+				t.Fatalf("Want code: %d, Get code: %d", test.WantCode, code)
+			}
+
+			if test.WantCode == http.StatusOK {
+				if !strings.Contains(string(data), test.WantSnippet.Title) || !strings.Contains(string(data), test.WantSnippet.Content) {
+					t.Fatalf("Want see: %v", test.WantSnippet)
+				}
+			}
+		})
+	}
+}

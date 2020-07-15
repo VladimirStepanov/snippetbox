9512fbb488b75b28f5315a202f3d56017f8f27f7
Vladimir Stepanov <vovakirishi@gmail.com>
2020-07-15 23:36:01 +0300

    Add /snippet/delete/%d path

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -34,6 +34,7 @@ func (s *Server) routes() http.Handler {
 	r.PathPrefix("/static/").Handler(strPref)
 	r.HandleFunc("/", s.home).Methods("GET")
 	r.Handle("/snippets", s.accessOnlyAuth(http.HandlerFunc(s.userSnippets))).Methods("GET")
+	r.Handle("/snippet/delete/{id:[0-9]+}", s.accessOnlyAuth(http.HandlerFunc(s.deleteSnippet))).Methods("GET")
 	r.HandleFunc("/snippet/{id:[0-9]+}", s.showSnippet).Methods("GET")
 	r.Handle("/user/signup", s.accessOnlyNotAuth(http.HandlerFunc(s.signUpPOST))).Methods("POST")
 	r.Handle("/user/signup", s.accessOnlyNotAuth(http.HandlerFunc(s.signUp))).Methods("GET")
